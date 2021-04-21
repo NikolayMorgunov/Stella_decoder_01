df6b3c39ba556795d0e558f7f9d3a3b25bd1a75c
Darkkvod <da>
2021-04-21 21:58:32 +0300

    Fixed colour

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ samplerate, data = read(file)
 data_am = data.copy()
 data_am = hilbert(data_am)
 
-max_d = max(data_am)
-data_am = [i * 255 // max_d for i in data_am]
+max_d = max(data_am[314*5512:653*5512])
+min_d = min(data_am[314*5512:653*5512])
+print(max_d, min_d)
+data_am = [((i - min_d) * 255) // (max_d - min_d) for i in data_am]
 data_am, matrix = find_synchro(samplerate, data_am)
 
 draw(samplerate, data_am)
--- make_matrix.py
+++ make_matrix.py
@@ -1,6 +1,7 @@
 def to_matrix(fs, data):
     frame_width = int(0.5 * fs)
     w, h = frame_width, len(data) // frame_width
+    # print(w)
     matrix = []
 
     for i in range(314, 653):
